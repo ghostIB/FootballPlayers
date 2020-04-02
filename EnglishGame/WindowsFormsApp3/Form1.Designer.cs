cd7f30077d90429e353012029b708c84f7c13726
GhostIB <butsenko.ivansh@gmail.com>
2020-04-02 15:17:12 +0400

    add EnglisgGame project

---
+++ EnglishGame/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 225);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tiger";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(341, 379);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(179, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(638, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(418, 417);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(33, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox3.Image = global::WindowsFormsApp3.Properties.Resources._1;
+            this.pictureBox3.Location = new System.Drawing.Point(605, 234);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(152, 111);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown3);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove3);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp3);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox2.Image = global::WindowsFormsApp3.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(342, 234);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 111);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown2);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove2);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp2);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox1.Image = global::WindowsFormsApp3.Properties.Resources._1_282;
+            this.pictureBox1.Location = new System.Drawing.Point(73, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp1);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(307, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 225);
+            this.panel2.TabIndex = 1;
+            this.panel2.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(546, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(242, 225);
+            this.panel3.TabIndex = 1;
+            this.panel3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 49);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Enter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Wolf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(647, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sheep";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 170;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EnglishGame/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private bool mouse1 = false;
+        private bool mouse2 = false;
+        private bool mouse3 = false;
+        private int score = 0;
+        private int time = 0;
+        private Dictionary<PictureBox, Panel> data;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem="1";
+            progressBar1.Value = 100;
+            data = new Dictionary<PictureBox, Panel>()
+            {
+                {pictureBox1,panel1 },
+                {pictureBox2,panel2 },
+                {pictureBox3,panel3 },
+            };
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel_Hover(object sender, EventArgs e)
+        {
+            IsInPanel(pictureBox1, panel1);
+        }
+
+        private void Panel2_Hover(object sender, EventArgs e)
+        {
+            IsInPanel(pictureBox2, panel2);
+        }
+
+        private void MouseDown1(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            mouse1 = true;
+        }
+
+        private void MouseMove1(object sender, MouseEventArgs e)
+        {
+            Control c1 = sender as Control;
+            if (mouse1)
+            {
+                c1.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void MouseUp1(object sender, MouseEventArgs e)
+        {
+            mouse1 = false;
+        }
+
+        private void MouseDown2(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            mouse2 = true;
+        }
+
+        private void MouseMove2(object sender, MouseEventArgs e)
+        {
+            Control c2 = sender as Control;
+            if (mouse2)
+            {
+                c2.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void MouseUp2(object sender, MouseEventArgs e)
+        {
+            mouse2 = false;
+        }
+
+        private void MouseMove3(object sender, MouseEventArgs e)
+        {
+            Control c3 = sender as Control;
+            if (mouse3)
+            {
+                c3.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void MouseUp3(object sender, MouseEventArgs e)
+        {
+            mouse3 = false;
+        }
+
+        private void MouseDown3(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            mouse3 = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void Timer1_Tick_1(object sender, EventArgs e)
+        {
+            progressBar1.Value --;
+            time++;
+            if (progressBar1.Value==1)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show($"Ваш счет: {score / time * 100}");
+                Close();
+            }
+        }
+        public static bool IsInPanel(PictureBox TImage, Panel TShape)
+        {
+            if (TImage.Left >= TShape.Left && TImage.Left + TImage.Width <= TShape.Left + TShape.Width && TImage.Top >= TShape.Top && TImage.Top + TImage.Height <= TShape.Top + TShape.Height)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<PictureBox,Panel> keyValue in data)
+            {
+                if (IsInPanel(keyValue.Key,keyValue.Value))
+                {
+                    score++;
+                }
+            }
+            MessageBox.Show($"Ваш счет: {score*1000/time}");
+            Close();
+        }
+    }
+}
